2df5301e838a4b2dff4cd57e79c7104a8a33d5ee
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-07-31 08:55:00 +0200

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,22 @@
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+
+    long long zeroFilledSubarray(vector<int>& nums) {
+        int index = 0;
+        long long zeroFilledSubarrays = 0;
+
+        while (index < nums.size()) {
+            long countConsecutiveZeros = 0;
+            while (index < nums.size() && nums[index++] == 0) {
+                ++countConsecutiveZeros;
+            }
+            //sum of all elements of arithmetic progression with first member '1' and step of increase '1'.
+            zeroFilledSubarrays += (1 + countConsecutiveZeros) * countConsecutiveZeros / 2;
+        }
+        return zeroFilledSubarrays;
+    }
+};
